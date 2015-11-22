feab85e581f64e64be4d310bb1e0d996724a376d
Edgar Liberis <edgaras.liberis@gmail.com>
2015-11-22 17:11:55 +0000

    Ex 5, 10

--- DSP/Supervision1/Supervision.m
+++ DSP/Supervision1/Supervision.m
@@ -29,13 +29,14 @@
 %      | 0     0     0     0     1 |
 
 % b)
-b = [1; 0; 0; 2; 2];
+f45 = [1; 0; 0; 2; 2];
 A_star = toeplitz([-3, 2, 1, 0, 0, 0, 0], [-3, 0, 0, 0, 0]);
-c = (A_star * b)' % Transpose for printing in 1 row
-
-% c)
+c = (A_star * f45)' % Transpose for printing in 1 row
 
 conv([-3, 2, 1], [1, 0, 0, 2, 2])
+
+% c) Solving for b
+round(linsolve(A_star, c'))'
 
 %%
 % *Exercise 7* 
@@ -52,3 +53,32 @@ conv([-3, 2, 1], [1, 0, 0, 2, 2])
 %%
 % *Exercise 10*
 % 
+
+% a)
+% 1 sec long 300Hz Gaussian noise
+noise = randn(300, 1); % Isn't randn uniform, as opposed to Gaussian?
+
+% Band-pass filter of 45Hz cut-off
+f45 = fir1(50, 45/150);
+x = filtfilt(f45, 1, noise);
+
+% Sample X at 100Hz
+y = zeros(1, 300);
+y(1:3:end) = x(1:3:end);
+
+% Band-pass filter of 50Hz cut-off
+f50 = fir1(50, 50/150);
+z = filtfilt(f50, 1, y) .* 3; % Compensate energy loss
+
+hFig = figure(1);
+set(hFig, 'Position', [20 20 1000 600])
+plot(x, 'b'); hold on;
+plot(y, 'r'); hold on;
+plot(z, 'g');
+legend('x_n = 45Hz bandpassed 300Hz noise', ...
+       'y_n = x_n sampled at 100Hz', ...
+       'z_n = 50Hz bandpassed y_n');
+
+% b) So that the second filter doesn't discard useful information. 
+% Bandpassing signal whose highest frequency is 45Hz with a 50Hz
+% filter would have no effect.
